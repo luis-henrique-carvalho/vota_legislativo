88de0bd6474af35a13ad38fe42a5938755310a61
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-09-09 20:12:23 -0300

    :recycle: reafactor: user models

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,35 +1,79 @@
 export enum UserRole {
-    Adm = "adm",
-    Alderman = "vereador",
-    TV = "tv",
-    Vereador = "vereador",
-    Messario = "mesaria",
-  }
-  
-  export interface User {
-    id?: string;
-    name: string;
-    email: string;
-    password: string;
-    funcao: string;
-    partido: string;
-    rg: string;
-    cpf: string;
-    titulo_eleitor: string;
-    zona_titulo: string;
-    telefone: string;
-    escolaridade: string;
-    data_expedicao_rg: string;
-    data_nascimento: string;
-    tipo?: string;
-    password_confirmation?: string;
-    old_password?: string;
-    created_at?: string;
-    updated_at?: string;
-    avatar_url?: string
-  }
-  export interface UserLogin {
-    email: string;
-    password: string;
-  }
-  
+  Adm = "adm",
+  Alderman = "vereador",
+  TV = "tv",
+  Vereador = "vereador",
+  Messario = "mesaria",
+}
+
+export interface User {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  funcao: string;
+  partido: string;
+  rg: string;
+  cpf: string;
+  titulo_eleitor: string;
+  zona_titulo: string;
+  telefone: string;
+  escolaridade: string;
+  data_expedicao_rg: string;
+  data_nascimento: string;
+  tipo?: string;
+  password_confirmation?: string;
+  old_password?: string;
+  created_at?: string;
+  updated_at?: string;
+  avatar_url?: string;
+}
+export interface UserLogin {
+  email: string;
+  password: string;
+}
+
+export type AldermanFunctions =
+  | ""
+  | "Vereador"
+  | "Presidente"
+  | "Administrador"
+  | "Mesário"
+  | "Prefeito";
+
+// SELECTIONS VALUES
+
+export const roleOptions = [
+  { value: "", label: "Selecionar" },
+  { value: "Vereador", label: "Vereador" },
+  { value: "Presidente", label: "Presidente" },
+  { value: "Administrador", label: "Administrador" },
+  { value: "Mesário", label: "Mesário" },
+  { value: "Prefeito", label: "Prefeito" },
+];
+
+export const typeOptions = [
+  { value: "", label: "Selecionar" },
+  { value: "vereador", label: "Vereador" },
+  { value: "mesaria", label: "Mesaria" },
+  { value: "tv", label: "TV" },
+];
+
+export const educationOptions = [
+  { value: "", label: "Selecionar" },
+  {
+    value: "Ensino Fundamental Incompleto",
+    label: "Ensino Fundamental Incompleto",
+  },
+  {
+    value: "Ensino Fundamental Completo",
+    label: "Ensino Fundamental Completo",
+  },
+  { value: "Ensino Médio Incompleto", label: "Ensino Médio Incompleto" },
+  { value: "Ensino Médio Completo", label: "Ensino Médio Completo" },
+  {
+    value: "Ensino Superior Incompleto",
+    label: "Ensino Superior Incompleto",
+  },
+  { value: "Ensino Superior Completo", label: "Ensino Superior Completo" },
+];
