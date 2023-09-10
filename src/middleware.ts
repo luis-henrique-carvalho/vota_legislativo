cf156f5626aaee1df29e47935e34c9f2f2809f2c
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-09-09 21:48:14 -0300

    :bricks: ci: Updated middleware to handle user type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { User } from "./models/User";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
+  const userString = request.cookies.get("user")?.value;
+
+  // Verifique se userString é uma string não vazia antes de fazer o parse
+  const user: User | null = userString ? JSON.parse(userString) : null;
 
   const loginUrl = new URL("/login", request.url);
   const aldermanUrl = new URL("/alderman", request.url);
+  const adminUrl = new URL("/admin", request.url);
 
   if (!token) {
     if (request.nextUrl.pathname === "/login") {
@@ -14,8 +20,25 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(loginUrl);
   }
 
-  if (token && request.nextUrl.pathname === "/login") {
-    return NextResponse.redirect(aldermanUrl);
+  switch (user?.tipo) {
+    case "vereador": {
+      if (request.nextUrl.pathname === "/admin") {
+        return NextResponse.redirect(aldermanUrl);
+      }
+
+      if (request.nextUrl.pathname === "/login") {
+        return NextResponse.redirect(aldermanUrl);
+      }
+    }
+    case "mesaria": {
+      if (request.nextUrl.pathname === "/alderman") {
+        return NextResponse.redirect(adminUrl);
+      }
+
+      if (request.nextUrl.pathname === "/login") {
+        return NextResponse.redirect(adminUrl);
+      }
+    }
   }
 }
 
